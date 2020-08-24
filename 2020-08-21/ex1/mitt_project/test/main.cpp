f232b87e8821de3eaf94c4d5f25701acd916aa72
posktomten <ic_0002@ceicer.com>
2020-08-24 16:23:43 +0200

    Lektion 6

--- 2020-08-21/ex1/mitt_project/test/main.cpp
+++ 2020-08-21/ex1/mitt_project/test/main.cpp
@@ -9,11 +9,14 @@
 using namespace std;
 // int main, int betyder ett heltal
 // ett heltal skall returneras från programmet till operativsystemet
+// "returneras" kan översättas med "skickas från prgrammet"
+// eller "kommer från programmet" och skickas till operativsystemet, Windows
 int main()
 {
     // cout=standard utmatningsenhet, skärmen
     cout << "Hello world!" << endl; // Text skall omges med ""
-    cout << (6/3) << endl; // endl= ny rad
-    //cout << (5/0) << endl;
-    return 0;
+    cout << (6/3) << endl; // endl=ny rad
+    //cout << (5/0) << endl; // Här händer det något förbudet, division med 0
+    // "Ett heltal, skilt från 0" kommer att returneras 
+	return 0; // Tar vi bort kommenteringen från rad 19, kommer aldrig programmet hit.
 }
