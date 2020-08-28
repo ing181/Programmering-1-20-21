d0d0fbfb28dee755f4eaa837bb68bfae3591bb7d
posktomten <ic_0002@ceicer.com>
2020-08-28 09:11:03 +0200

    modulus

---
+++ 2020-08-28/numeriska_operatorer/num/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Numeriska operatorer
+    // +, -, /, * och % (modulus)
+    int tal = 2.99;
+    cout << "tal= " << tal << endl;
+    int a = 5;
+    int b = 3;
+    // Returnerar heltalsdelen
+    cout << (a/b) << endl;
+    // Returnerar resten
+    cout << (a%b) << endl;
+    return 0;
+}
