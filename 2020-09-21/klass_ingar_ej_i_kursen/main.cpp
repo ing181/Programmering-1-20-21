87b9acc6066d3045cf3fd093f70be3bfa1e712e5
posktomten <ic_0002@ceicer.com>
2020-09-21 14:26:43 +0200

    Kommentarer

--- 2020-09-21/klass_ingar_ej_i_kursen/main.cpp
+++ 2020-09-21/klass_ingar_ej_i_kursen/main.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
-
 using namespace std;
-
 class Alder {
 
-    private:
+private:
+    // variabeln alder kan bara kommas åt innifrån klassen
     int alder;
-    public:
-    void setAlder(int years) {
-        alder = years;
+public:
+    // Public, går att komma åt utanför klassen Alder
+    void setAlder ( int years )
+    {
+        alder = years;  // Här sker tilldelningen
+                        // innifrån klassen, av en metod som anropats utifrån
     }
 
-        int getAlder() {
+    int getAlder()  // Returnerar innehåller i den privata variabeln alder
+    {
         return alder;
     }
-
-
 };
 
 int main()
 {
     cout << "Hello world!" << endl;
     int myAge;
-    Alder myAlder;
-
+    Alder myAlder; // En instans av, "en Alder" deklareras
+                   // Se klassen som en ritning, du skapar ett objekt
+                   // av klassen Alder. Objekt finns, klasser är ritningar.
+				   // Deklareras på samma sätt som en int eller en double eller 
+				   // någon annan datatyp.
     cout << "Hur gammal är du? ";
     cin >> myAge;
-    myAlder.setAlder(myAge);
-    cout << myAlder.getAlder();
-
+    myAlder.setAlder ( myAge ); // Metoden setAlder som ingår i klassen Alder används
+    cout << myAlder.getAlder(); // Metoden getAlder som ingår i klassen Alder används
     return 0;
 }
